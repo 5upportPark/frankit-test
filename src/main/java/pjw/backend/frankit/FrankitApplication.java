a2ba5659c324a3837c769a4678e6eea07af02686
5upportPark <5upportpark7@gmail.com>
2025-02-19 21:12:06 +0900

    init

---
+++ src/main/java/pjw/backend/frankit/FrankitApplication.java
@@ -0,0 +1,13 @@
+package pjw.backend.frankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrankitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pjw/backend/frankit/FrankitApplicationTests.java
@@ -0,0 +1,13 @@
+package pjw.backend.frankit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrankitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
